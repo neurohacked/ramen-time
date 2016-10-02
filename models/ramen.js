cade70799c7419406fbe189856a61bceceebc921
neurohacked <admin@neurohacked.com>
2016-10-01 23:18:41 -0700

    Fix function names.
    Fix functions to match orm.

--- models/ramen.js
+++ models/ramen.js
@@ -16,12 +16,12 @@ var ramen = {
 			cb(res);
 		});
 	},
-	update: function (objColVals, condition, cb) {
+	modify: function (objColVals, condition, cb) {
 		orm.update('ramen', objColVals, condition, function (res) {
 			cb(res);
 		});
 	},
-	delete: function (condition, cb) {
+	scrap: function (condition, cb) {
 		orm.delete('ramen', condition, function (res) {
 			cb(res);
 		});
